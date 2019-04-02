ff364366a7b63894c4f4362d4db96459afe43319
BraeWebb <email@braewebb.com>
2019-04-02 13:53:05 +1000

    Sample Annotations

---
+++ src/main/java/chalkbox/api/annotations/DataSet.java
@@ -0,0 +1,13 @@
+package chalkbox.api.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface DataSet {
+    String stream();
+    Class type();
+}
