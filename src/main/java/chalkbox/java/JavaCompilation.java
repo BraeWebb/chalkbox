acb46dc2cde6cd231d358232174c7982314a2f4b
BraeWebb <email@braewebb.com>
2019-04-15 20:49:56 +1000

    Refractored Java Compiler

--- src/main/java/chalkbox/java/JavaCompilation.java
+++ src/main/java/chalkbox/java/JavaCompilation.java
@@ -1,74 +1,76 @@
 package chalkbox.java;
 
-import chalkbox.api.annotations.Asset;
+import chalkbox.api.annotations.ConfigItem;
 import chalkbox.api.annotations.Pipe;
 import chalkbox.api.annotations.Processor;
 import chalkbox.api.collections.Bundle;
 import chalkbox.api.collections.Collection;
 import chalkbox.api.collections.Data;
-import chalkbox.api.common.Compiler;
-import chalkbox.api.files.SourceFile;
+import chalkbox.api.common.java.Compiler;
 
 import javax.tools.JavaFileObject;
-import java.io.IOException;
 import java.io.StringWriter;
-import java.util.Arrays;
-import java.util.Map;
 
+/**
+ * Process to compile all of the .java source files in a submission.
+ *
+ * <p>A compiling submission may produce the following output, where ... stands
+ * for any output warnings when compiling:
+ * <pre>
+ * "compilation": {
+ *     "compiles": true,
+ *     "output": "..."
+ * }
+ * </pre>
+ *
+ * <p>A non-compiling submission may produce the following output, where ... stands
+ * for the compilation errors:
+ * <pre>
+ * "compilation": {
+ *     "compiles": false,
+ *     "output": "..."
+ * }
+ * </pre>
+ *
+ * <p>A submission without any .java files will have compilation.compiles set
+ * to false and compilation.output set to "Empty submission"
+ */
 @Processor
 public class JavaCompilation {
-    private Bundle working;
-    private Bundle source;
-    private Data results;
-
+    @ConfigItem(description = "Class path to use to compile submissions")
     private String classPath;
 
-    @Asset
-    public void getClassPath(Map<String, String> config) {
-        classPath = config.get("classPath");
-    }
 
     @Pipe(stream = "submissions")
     public Collection compile(Collection submission) {
-        working = submission.getWorking();
-        source = submission.getSource();
-        results = submission.getResults();
+        Bundle working = submission.getWorking();
+        Data results = submission.getResults();
 
-        // Create a new output directory
-        if (!makeOutputDirectory()) {
+        results.set("compilation.compiles", false);
+
+        /* Attempt to create a new output directory */
+        if (!working.makeDir("bin")) {
+            System.err.println("Couldn't create output directory");
+            results.set("compilation.error", "Couldn't create output directory - see tutor");
+            return submission;
+        }
+
+        Iterable<? extends JavaFileObject> sourceFiles = Compiler.getSourceFiles(submission.getSource());
+        if (sourceFiles == null) {
+            System.err.println("Couldn't load source files");
+            results.set("compilation.error", "Error loading source files - see tutor");
             return submission;
         }
 
         StringWriter output = new StringWriter();
-        classPath = source.getUnmaskedPath("") + ":" + classPath;
+        String classPath = submission.getSource().getUnmaskedPath() + ":" + this.classPath;
 
-        boolean success = Compiler.compile(loadSourceFiles(), classPath,
+        boolean success = Compiler.compile(sourceFiles, classPath,
                 working.getUnmaskedPath("bin"), output);
 
         results.set("compilation.compiles", success);
         results.set("compilation.output", output.toString());
         working.refresh();
         return submission;
-    }
-
-    private boolean makeOutputDirectory() {
-        if (!working.makeDir("bin")) {
-            results.set("compilation.compiles", false);
-            results.set("compilation.error", "Couldn't create output directory - see tutor");
-            return false;
-        }
-        return true;
-    }
-
-    private Iterable<? extends JavaFileObject> loadSourceFiles() {
-        SourceFile[] sourceFiles;
-        try {
-            sourceFiles = source.getFiles(".java");
-        } catch (IOException e) {
-            results.set("compilation.compiles", false);
-            results.set("compilation.error", "Error loading source files - see tutor");
-            return null;
-        }
-        return Arrays.asList(sourceFiles);
     }
 }
