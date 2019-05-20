51b6b5cbcbc07ded571037aae3e9eed7cb62dd66
BraeWebb <email@braewebb.com>
2019-05-20 20:08:14 +1000

    Flagging Identified Broken Solutions
    Hacky solution to flag solutions that were identified as broken.

--- src/main/java/chalkbox/java/junit/JUnit.java
+++ src/main/java/chalkbox/java/junit/JUnit.java
@@ -19,6 +19,9 @@ import java.util.Map;
 
 @Processor
 public class JUnit {
+    private static final String SOLUTIONS_ROOT = "junit.solutions";
+    private static final String SAMPLE_SOLUTION = "solution";
+
     private String solutionClassPath;
     private Map<String, String> classPaths = new HashMap<>();
 
@@ -99,6 +102,57 @@ public class JUnit {
                 Data results = JUnitRunner.runTest(testClass, classPath);
                 String jsonRoot = "junit.solutions." + solution + "." + testClass.replace(".", "\\.");
                 submission.getResults().set(jsonRoot, results);
+            }
+        }
+
+        return submission;
+    }
+
+    /**
+     * Set a flag on each of the broken solutions to indicate whether it
+     * is correctly identified or not.
+     *
+     * TODO: Fix this so the flag is set when the test is run
+     */
+    @Pipe(stream = "submissions")
+    public Collection hack(Collection submission) {
+        Data data = submission.getResults();
+        /* Determine the baseline amount of tests that pass for the sample solution */
+        Map<String, Integer> baseline = new HashMap<>();
+        String solutionPath = SOLUTIONS_ROOT + "." + SAMPLE_SOLUTION;
+        for (String clazz : data.keys(solutionPath)) {
+            String solutionKey = solutionPath + "." + clazz.replace(".", "\\.");
+
+            if (data.get(solutionKey + ".passes") == null) {
+                continue;
+            }
+
+            int passes = Integer.parseInt(data.get(solutionKey + ".passes").toString());
+            baseline.put(clazz, passes);
+        }
+
+        /* Flag all the solutions that have a better amount of failing tests */
+        for (String solution : data.keys(SOLUTIONS_ROOT)) {
+            if (solution.equals(SAMPLE_SOLUTION)) {
+                continue;
+            }
+            String solutionKey = SOLUTIONS_ROOT + "." + solution;
+
+            for (String clazz : data.keys(solutionKey)) {
+                String testKey = solutionKey + "." + clazz.replace(".", "\\.");
+                data.set(testKey + ".correct", false);
+
+                if (data.get(testKey + ".passes") == null) {
+                    continue;
+                }
+
+                int passes = Integer.parseInt(data.get(testKey + ".passes").toString());
+
+                if (baseline.containsKey(clazz)) {
+                    if (passes < baseline.get(clazz)) {
+                        data.set(testKey + ".correct", true);
+                    }
+                }
             }
         }
 
